e8f4adbd5732b0140ac2f1782455e7a0e45d156b
croussey10 <charlie.roussey@coda-student.school>
2026-04-01 16:28:01 +0200

    refacto + root css

--- src/services/themeService.tsx
+++ src/services/themeService.tsx
@@ -6,18 +6,14 @@ export interface ThemeStat {
 }
 
 export const getThemeStats = async (userId: string, order: 'ASC' | 'DESC') => {
-    let query =  supabase
+    let query = supabase
         .from('theme_stats_view')
         .select('theme, moyenne_points')
         .eq('profile_id', userId);
 
-    if (order == 'ASC') {
-        query = query.order('moyenne_points', { ascending: true })
-    } else {
-        query = query.order('moyenne_points', { ascending: false })
-    }
+    query = query.order('moyenne_points', {ascending: order == 'ASC'})
 
-    const { data, error } = await query.limit(3);
+    const {data, error} = await query.limit(3);
 
     if (error) {
         console.error("Erreur lors de la récupération des pires notes :", error);
