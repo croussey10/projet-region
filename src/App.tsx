72d0b3f5596aaffcbcdb98b12f0092b317252041
Hugo Mebold <hugo.mebold@coda-student.school>
2026-04-01 11:00:34 +0200

    add login + register

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {Home} from "./pages/home.tsx";
 import {Login} from "./pages/login.tsx";
 import {Signup} from "./pages/signup.tsx";
 import {Dashboard} from "./pages/dashboard.tsx";
+import {Logintest} from "./pages/logintest.tsx";
+import {Signuptest} from "./pages/signuptest.tsx";
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/signup" element={<Signup/>}/>
                     <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/logintest" element={<Logintest/>}/>
+                    <Route path="/signuptest" element={<Signuptest/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
